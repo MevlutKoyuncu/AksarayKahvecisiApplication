fd9ea28354d9efb60f76577486cc6754742e101f
MevlutKoyuncu <mevoresko@gmail.com>
2024-10-27 23:41:43 +0300

    .

--- AksarayKahvecisiApplication/AdminLoginPanel/YoneticiMaster.Master.designer.cs
+++ AksarayKahvecisiApplication/AdminLoginPanel/YoneticiMaster.Master.designer.cs
@@ -51,6 +51,15 @@ namespace AksarayKahvecisiApplication.AdminLoginPanel
         protected global::System.Web.UI.WebControls.Label lbl_kullanici;
 
         /// <summary>
+        /// ContentPlaceHolder2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder2;
+
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
